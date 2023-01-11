7d30af05b22f27016b919093f7ae5a30c4475b22
Jonathan <114364831+jjlin526@users.noreply.github.com>
2023-01-11 09:22:04 -0500

    Update Greetings.js

--- Greetings.js
+++ Greetings.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 
+// with props and the basic Text, Image, and View components, you can build a wide varity of static screens
+// need state to make your app change over time
+
 const styles = StyleSheet.create({
   center: {
     alignItems: 'center'
   }
 })
 
+// using name as a prop lets us customize the Greeting component (reusable component)
 const Greeting = (props) => {
   return (
+    // View is a useful container for other components, to help control style and layout
     <View style={styles.center}>
       <Text>Hello {props.name}!</Text>
     </View>
