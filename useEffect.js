831a4eabc7f78fa5b1e5ff0e65ede75f0d1de9f8
Jonathan <114364831+jjlin526@users.noreply.github.com>
2023-01-11 16:17:08 -0500

    Update useEffect.js

--- useEffect.js
+++ useEffect.js
@@ -13,6 +13,27 @@ When our component function runs, the callback will be called if any dependencie
 
 */
 
+import React, { useState, useEffect } from 'react'
+import { Button } from 'react-native'
+
+export default function App() {
+  const [count, setCount] = useState(0)
+  const countEvery3 = Math.floor(count / 3)
+
+  useEffect(() => {
+    console.log(countEvery3)
+  }, [countEvery3])
+
+  return (
+    <Button
+      title={`Increment ${count}`}
+      onPress={() => {
+        setCount(count + 1)
+      }}
+    />
+  )
+}
+
 /*
 
 In this example, we will log to the console any time the count is a multiple of 3. The callback is called every time the countEvery3 changes, since countEvery3 is listed as a dependency.
