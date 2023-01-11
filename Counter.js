6983f7c1dc3db957b1fa841d39f1b73d38809b16
Jonathan <114364831+jjlin526@users.noreply.github.com>
2023-01-11 09:35:04 -0500

    Create Counter.js

---
+++ Counter.js
@@ -0,0 +1,12 @@
+// React Native Counter Example using Hooks
+
+import React, {useState} from 'react';
+import {View, Text, Button, StyleSheet} from 'react-native';
+
+const App = () => {
+   const [count, Setcount] = useState(0);
+  
+  return (
+    
+  )
+}
