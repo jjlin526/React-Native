15fb004db17b804efc965832503d53aa5b5e0d9e
Jonathan <114364831+jjlin526@users.noreply.github.com>
2023-01-11 10:03:12 -0500

    Update Counter.js

--- Counter.js
+++ Counter.js
@@ -1,12 +1,27 @@
-// React Native Counter Example using Hooks
+// React Native Counter Example using Hooks!
 
 import React, {useState} from 'react';
 import {View, Text, Button, StyleSheet} from 'react-native';
 
 const App = () => {
-   const [count, Setcount] = useState(0);
-  
+  const [count, setCount] = useState(0);
+
   return (
-    
-  )
-}
+    <View style={styles.container}>
+      <Text>You clicked {count} times</Text>
+      <Button
+        onPress={() => setCount(count + 1)}
+        title="Click me!"
+      />
+    </View>
+  );
+};
+
+// React Native Styles
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
